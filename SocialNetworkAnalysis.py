f30ec02528484a1a19661efa38b24df267894048
undecidedarmchairexpert <undecidedarmchairexpert@gmail.com>
2020-03-13 01:52:05 -0400

    Create and Format Output File

--- SocialNetworkAnalysis.py
+++ SocialNetworkAnalysis.py
@@ -29,9 +29,42 @@ def interactive_graph(graph, prd_mode=False):
     # If not work in production mode, show buttons
     if not prd_mode:
         sna_visualisation.show_buttons()
+    if prd_mode:
+        sna_visualisation.set_options("""
+            var options = {
+              "nodes": {
+                "borderWidth": 3,
+                "color": {
+                  "border": "rgba(0,0,0,1)",
+                  "background": "rgba(103,116,127,1)",
+                  "highlight": {
+                    "border": "rgba(0,0,0,1)",
+                    "background": "rgba(252,255,252,1)"
+                  }
+                },
+                "font": {
+                  "size": 16,
+                  "strokeWidth": 5
+                },
+                "size": 10
+              },
+              "edges": {
+                "color": {
+                  "inherit": true
+                },
+                "smooth": false
+              },
+              "physics": {
+                "barnesHut": {
+                  "gravitationalConstant": -23350
+                },
+                "minVelocity": 0.75
+              }
+            }
+        """)
 
     # Save web page and return graph object
-    return sna_visualisation.show("Data/sna.html")
+    return sna_visualisation.show("sna.html")
 
 
 # Creates a friend group in an nx graph from a "core" and an "extended" graph
@@ -39,29 +72,31 @@ def interactive_graph(graph, prd_mode=False):
 # The extended list is connected to all nodes, but with a weight of 1
 # This function does not return anything
 # The graph to which the objects are added is passed to the function and added directly
-def create_friend_group(graph, core_list, ext_list=None):
-    # Mutable object
+def create_friend_group(graph, core_list=None, ext_list=None):
+    # Mutable object check
     if ext_list is None:
         ext_list = []
+    if core_list is None:
+        core_list = []
     # Iterate over core list and add links
     for person1 in core_list:
         for person2 in core_list:
             if person1 != person2:
-                graph.add_edge(person1, person2, weight=5)
+                graph.add_edge(person1, person2, weight=5, color="#00b4ff")
         for person2 in ext_list:
-            graph.add_edge(person1, person2, weight=1)
+            graph.add_edge(person1, person2, weight=1, color="#00b4ff")
     # Iterate over extended list and add links between each other
     for person1 in ext_list:
         for person2 in ext_list:
             if person1 != person2:
-                graph.add_edge(person1, person2, weight=1)
+                graph.add_edge(person1, person2, weight=1, color="#00b4ff")
 
 
 # Start of script
 # Determine run mode from arguments passed to the scrips
 # Default is to run as production
 # If the "notPrd" flag is passed, script will run as testing
-if (sys.argv[1]) == "notPrd":
+if "notPrd" in sys.argv:
     prd = False
 else:
     prd = True
@@ -71,7 +106,7 @@ sna = nx.Graph()
 
 # Code containing graph data is kept in a separate folder
 # Add data to graph here
-exec(open("Data/graph_data.txt").read())
+exec(open("Data/graph_data.py").read())
 
 # Draw interactive graph
 # Check if running in production mode otherwise show buttons
